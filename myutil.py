3093766525319bf35a3d344a0890666ee2163833
hoey <nznial@gmail.com>
2015-05-24 10:32:47 +0900

    player made

--- myutil.py
+++ myutil.py
@@ -1,2 +1,11 @@
+from kivy.vector     import Vector
+
+KEY_DIRECTION = { \
+    'left'  : Vector(-1, 0), \
+    'right' : Vector(1, 0), \
+    'up'    : Vector(0, 1), \
+    'down'  : Vector(0, -1) \
+}
+
 def is_arrowkey(k):
-    return (k == 'left') or (k == 'right') or (k == 'up') or (k == 'down')
+    return (k in KEY_DIRECTION)
--- zombie.py
+++ zombie.py
@@ -1,10 +1,13 @@
 from kivy.uix.widget import Widget
 from kivy.vector     import Vector
 
+from myutil import *
+
+
 class Zombie(Widget):
+
     def build(self):
         self.accs = set()
-        self.vel = Vector(0, 0)
 
     def acc(self, arrow):
         self.accs.add(arrow)
@@ -13,4 +16,11 @@ class Zombie(Widget):
         self.accs.remove(arrow)
 
     def update(self):
-        self.pos = self.vel + self.pos
+        self.pos = self._direction()*10 + self.pos
+
+    def _direction(self):
+        if not self.accs:
+            return Vector(0,0)
+        directions = [KEY_DIRECTION[d] for d in self.accs]
+        return reduce(lambda x,y: x+y, directions).normalize()
+
