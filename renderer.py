dc11be69b6930cc2065c43ec8117be15748a2073
hoey <nznial@gmail.com>
2015-05-31 18:00:59 +0900

    stencil

--- renderer.py
+++ renderer.py
@@ -1,37 +1,31 @@
 #from kivy.core.window import Window
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.graphics           import Rectangle, Ellipse, Line, Color
+#from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.boxlayout   import BoxLayout
+from kivy.uix.stencilview import StencilView
+from kivy.graphics        import Rectangle, Ellipse, Line, Color
 
 from myutil   import *
 from zombie   import Zombie
 from man      import Man
 
 
-class Renderer(RelativeLayout):
+#class Renderer(RelativeLayout):
+class Renderer(BoxLayout, StencilView):
     def update(self, world, dt):
         self.canvas.clear()
         with self.canvas:
             for walker in world.walkers:
-
-                pos       = walker.position
+                pos       = walker.position+self.pos
                 rad, rad2 = walker.radius, walker.radius*2
 
-                def is_inboundex():
-                    x, y   = pos
-                    bx, by = world.size
-                    return x-rad>=0 and x+rad<=bx and y-rad>=0 and y+rad<=by
-
-                if not is_inboundex(): continue
-
                 Color(1, 1, 1)
-                center = walker.position - Vector(rad, rad)
+                center = pos - Vector(rad, rad)
                 Ellipse(pos=center, size=(rad2, rad2))
 
                 Color(1, 0, 0)
                 dirlen = rad*1.5
                 points = [pos]+[pos+walker.direction*dirlen]
                 Line(points=[e for p in points for e in p])
-
 
                 Color(0, 1, 0)
                 vellen = rad*1.8
--- settings.py
+++ settings.py
@@ -6,12 +6,12 @@ from kivy.vector     import Vector
 
 
 #MAN_SIGHT = MAN_SPEED*7
-#MAN_AVOID = MAN_SPEED*5
 
 class Settings(EventDispatcher):
     world_size    = ObjectProperty(Vector(100, 100))
     use_avoidance = BooleanProperty(True)
-    walker_count  = BoundedNumericProperty(30, min=1, max=200)
+    walker_count  = BoundedNumericProperty(10, min=0, max=200)
+
     walker_radius = BoundedNumericProperty(dp(20), min=dp(5),  max=dp(100))
-    walker_speed  = BoundedNumericProperty(dp(30), min=dp(1),  max=dp(200))
+    walker_speed  = BoundedNumericProperty(dp(50), min=dp(1),  max=dp(200))
     walker_force  = BoundedNumericProperty(dp(50), min=dp(10), max=dp(2500))
